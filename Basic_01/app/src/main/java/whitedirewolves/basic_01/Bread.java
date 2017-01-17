d7d95b5430e2d1a6f18d5e1f82dc925fef61959e
Junior <jcupe.cas@gmail.com>
2017-01-17 20:02:13 -0200

    Lecture 02 Project Basic 01

---
+++ Basic_01/app/src/main/java/whitedirewolves/basic_01/Bread.java
@@ -0,0 +1,10 @@
+package whitedirewolves.basic_01;
+
+/**
+ * Created by juni on 17/01/17.
+ */
+
+public interface Bread {
+    String name();
+    String calories();
+}
---
+++ Basic_01/app/src/main/java/whitedirewolves/basic_01/Filling.java
@@ -0,0 +1,10 @@
+package whitedirewolves.basic_01;
+
+/**
+ * Created by juni on 17/01/17.
+ */
+
+public interface Filling {
+    String name();
+    String calories();
+}
